c268d666d8c207234d641efb24568183263b80c5
Bimba Shrestha <bshrestha.msae@gmail.com>
2024-12-18 10:31:33 -0500

    Add create-dmg script

---
+++ electron/create-dmg.sh
@@ -0,0 +1,13 @@
+cd AIMINER-darwin-x64
+rm -rf AIMINER.dmg
+create-dmg \
+  --volname "AIMINER Installer" \
+  --window-pos 100 100 \
+  --window-size 300 440 \
+  --icon-size 75 \
+  --icon "AIMINER.app" 150 90 \
+  --background ../background.png \
+  --hide-extension "AIMINER.app" \
+  --app-drop-link 150 240 \
+  "AIMINER.dmg" \
+  "AIMINER.app"
