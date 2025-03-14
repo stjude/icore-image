74c023b681b6d7f3de4f275df8dd24882c858cc6
shermangriffiths <shermangriffiths@innolitics.com>
2025-03-13 23:00:13 -0500

    Update the `generate_license` command to output a multi-module license

--- deid/home/management/commands/generate_license.py
+++ deid/home/management/commands/generate_license.py
@@ -1,6 +1,7 @@
+import json
+
 from django.core.management.base import BaseCommand
 from home.license_management import LicenseManager
-from home.models import Project
 
 
 class Command(BaseCommand):
@@ -8,17 +9,17 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "module",
+            "license_info",
             type=str,
-            choices=[choice[0] for choice in Project.TaskType.choices],
-            help="Module name for which the license is being generated.",
-        )
-        parser.add_argument(
-            "expiration",
-            type=str,
-            help="Expiration date for the license in YYYY-MM-DD format.",
+            help=(
+                "A list of of information about what modules to license and the"
+                "expiration dates of their licenses. Format: "
+                '[{"modules": ["module_name", ...], "expiration": "YYYY-MM-DD"}, ...] '
+                f"Acceptable module names: {', '.join(LicenseManager.module_names)}"
+            ),
         )
 
     def handle(self, *args, **options):
-        LicenseManager().generate_license(options['module'], options['expiration'])
+        license_info = json.loads(options["license_info"])
+        LicenseManager().generate_license(license_info)
         self.stdout.write(self.style.SUCCESS("License generated successfully at `new_license.txt`."))
