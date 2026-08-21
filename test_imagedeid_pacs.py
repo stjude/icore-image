d2c17adeb386083e683446c5a48fe208cd79da69
Reece Stevens <reecestevens24@gmail.com>
2026-08-21 10:44:34 -0500

    Update tests after date formatting change

--- test_imagedeid_pacs.py
+++ test_imagedeid_pacs.py
@@ -496,24 +496,13 @@ def test_imagedeid_pacs_mrn_study_date_fallback(output_dir, appdata_dir, orthanc
     query_df_invalid = pd.DataFrame(query_data_invalid)
     query_df_invalid.to_excel(query_file_invalid, index=False)
 
-    query_spreadsheet_invalid = Spreadsheet.from_file(
-        str(query_file_invalid),
-        acc_col="AccessionNumber",
-        mrn_col="PatientID",
-        date_col="StudyDate",
-    )
-
-    with pytest.raises(ValueError, match="StudyDate must be in Excel date format"):
-        ImageDeidPacsPipeline(
-            pacs_list=[pacs_config],
-            query_spreadsheet=query_spreadsheet_invalid,
-            application_aet="TEST_AET",
-            output_dir=str(output_dir),
-            appdata_dir=str(appdata_dir),
-            storescp_port=orthanc.storescp_port,
-            cmove_batch_size=CMOVE_BATCH_SIZE,
-        ).run()
-
+    with pytest.raises(ValueError, match="time data.*doesn't match format"):
+        Spreadsheet.from_file(
+            str(query_file_invalid),
+            acc_col="AccessionNumber",
+            mrn_col="PatientID",
+            date_col="StudyDate",
+        )
 
 def test_imagedeid_pacs_date_window(output_dir, appdata_dir, orthanc):
     os.environ["DCMTK_HOME"] = str(Path(__file__).parent / "dcmtk")
